7e898c700ff7d4f32385553673fbfd5556501cd6
Denis Mazurak <pure.demicube@gmail.com>
2022-07-05 12:46:04 +0300

    Rename weather_app -> weather_site
    App in Django is not what I thought it was.

--- weather_site/urls.py
+++ weather_site/urls.py
@@ -1,4 +1,4 @@
-"""weather_app URL Configuration
+"""weather_site URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
