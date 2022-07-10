33cdd1c91cfa1df764bf9edd58f44c83efaa4497
Denis Mazurak <pure.demicube@gmail.com>
2022-07-10 20:51:29 +0300

    Remove secret key. It's stored in git commit history, but who cares :)

--- weather_site/settings.py
+++ weather_site/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h0n61d!)_5!rfzk%7vox(56aofmqsr-3-o0*_6y!tl039xfp*h'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,6 +82,7 @@ WSGI_APPLICATION = 'weather_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# Uses MySQL on prod
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
