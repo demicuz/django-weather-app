05a43b0a2c40a78eb3ca1fb58c92f817c7953f50
Denis Mazurak <pure.demicube@gmail.com>
2022-10-24 03:37:30 +0300

    Add views count for each city, use `OneToOneField` where possible

--- weather/admin.py
+++ weather/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import City, CurrentWeather
+from .models import City, CurrentWeather, CityStats
 
 admin.site.register(City)
 admin.site.register(CurrentWeather)
+admin.site.register(CityStats)
--- weather/models.py
+++ weather/models.py
@@ -24,7 +24,11 @@ class City(models.Model):
 
 
 class CurrentWeather(models.Model):
-    city        = models.ForeignKey(City, on_delete=models.CASCADE)
+    # city        = models.ForeignKey(City, on_delete=models.CASCADE)
+    city        = models.OneToOneField(City,
+                                       on_delete=models.CASCADE,
+                                       primary_key=True,
+                                       related_name="weather")
 
     temp        = models.FloatField(default=0)
     feels_like  = models.FloatField(default=0)
@@ -36,7 +40,6 @@ class CurrentWeather(models.Model):
     icon_name   = models.CharField(max_length=10)
 
     last_update = models.DateTimeField()
-
 
     def was_updated_last_minute(self):
         now = timezone.now()
@@ -66,9 +69,26 @@ class CurrentWeather(models.Model):
         return self.city.name + ": " + self.desc_short
 
 
+class CityStats(models.Model):
+    city  = models.OneToOneField(City,
+                                 on_delete=models.CASCADE,
+                                 primary_key=True,
+                                 related_name="stats")
+    views = models.IntegerField(default=0)
+
+    def inc_views(self):
+        self.views += 1
+        self.save()
+
+    class Meta:
+        verbose_name_plural = 'Статистика по городам'
+
+    def __str__(self):
+        return self.city.name + ": " + str(self.views)
+
+
 def create_current_weather(city, city_openweather_id):
     # TODO handle failed requests
-    # TODO hide OpenWeather API key
     url = 'http://api.openweathermap.org/data/2.5/weather?id={}&units=metric&appid={}'
     weather_json = requests.get(url.format(city_openweather_id, OPENWEATHER_KEY)).json()
     current_weather = CurrentWeather(
